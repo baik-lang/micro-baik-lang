4351c43123358f4e027c7f94f15e4a911c07d286
anak10thn <anak10thn@gmail.com>
2019-02-02 22:16:35 +0700

    ADD: copy url action

--- docs/main.js
+++ docs/main.js
@@ -61,3 +61,17 @@ function getAllUrlParams(url) {
 
     return obj;
 }
+
+function copyToClipboard(selector) {
+    /* Get the text field */
+    var copyText = document.getElementById(selector);
+
+    /* Select the text field */
+    copyText.select();
+
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+}
